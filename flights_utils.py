0cf9c9c1578e18885214160fe3a938782add2026
UNDI SURYA PAVANI <undisuryapavani@gmail.com>
2026-02-02 20:02:29 +0530

    Update flights_utils.py

--- flights_utils.py
+++ flights_utils.py
@@ -3,23 +3,22 @@ import streamlit as st
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("flights.csv.csv", nrows=70000)
+    # Converted Google Drive View link to Direct Download link
+    file_id = "19h0lEG2PouEG7sSM7MR6ARHncmYaIT6x"
+    data_url = f'https://drive.google.com/uc?id={file_id}'
+    
+    # Load data from URL
+    df = pd.read_csv(data_url, nrows=70000)
 
     df = df.dropna(subset=["DEP_DELAY"])
-
     df["IS_HIGH_RISK"] = df["DEP_DELAY"] > 15
     df["RISK_SCORE"] = df["IS_HIGH_RISK"].astype(int) * 100
 
     delay_cols = [
-        "DELAY_DUE_CARRIER",
-        "DELAY_DUE_WEATHER",
-        "DELAY_DUE_NAS",
-        "DELAY_DUE_SECURITY",
-        "DELAY_DUE_LATE_AIRCRAFT",
+        "DELAY_DUE_CARRIER", "DELAY_DUE_WEATHER", "DELAY_DUE_NAS",
+        "DELAY_DUE_SECURITY", "DELAY_DUE_LATE_AIRCRAFT",
     ]
-
     existing_cols = [c for c in delay_cols if c in df.columns]
-
     if existing_cols:
         df["TOTAL_DELAY"] = df[existing_cols].sum(axis=1)
 
